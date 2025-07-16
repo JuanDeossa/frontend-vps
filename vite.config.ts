d20373e5ecb1273d5054d8104e2453403e6c3a61
ricdeo718 <juandeossa.nc@gmail.com>
2025-07-16 03:18:13 +0000

    add docker for development

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Enable host mode 
+    watch: {
+      usePolling: true, // Enable polling for file changes
+      interval: 100, // Adjust the polling interval as needed
+    },
+  },
 })
